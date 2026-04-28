006baa478870fe435f45497feb43bdca63e51be4
bsc-inf-13-24 <bsc-inf-13-24@unima.ac>
2026-04-28 21:57:31 +0200

    polygon_pointers

--- polygon/main.cpp
+++ polygon/main.cpp
@@ -7,8 +7,11 @@ int main () {
     Rectangle rect;
     Triangle trgl;
 
-    rect.SetValues(4,5);
-    trgl.SetValues(4,5);
+    Polygon* pPoly1 = &rect;
+    Polygon* pPoly2 = &trgl;
+
+    pPoly1->SetValues(4,5);
+    pPoly2->SetValues(4,5);
 
     cout <<"rectangle area: "<<rect.area() << "\n";
     cout <<"triangle area: "<< trgl.area() << "\n";
